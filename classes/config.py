1c0ec4cec195a2c13b5c6cfdf7640d539e042f26
alexk-1998 <aekrosney@outlook.com>
2023-02-02 11:04:28 -0600

    added more band analysis

--- classes/config.py
+++
@@ -1,7 +0,0 @@
-bg_color = '#666666'        # primary application background colour
-widget_bg_color = '#bbbbbb' # secondary background colours, used for frames, scrollbar troughs, etc.
-hover_color = '#999999'     # colour used on mouse hover
-text_color = '#000000'      # colour used for text
-text_pad = 12               # padding around text used in tables
-relief = 'sunken'           # flat, groove, raised, ridge, solid, or sunken
-border_width = 2            # doesn't work with ttk.Frame, leave as 2
