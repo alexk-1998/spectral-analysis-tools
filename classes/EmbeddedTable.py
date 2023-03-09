26f04c12667289d3c3bc5031f0b14944138666f9
alexk-1998 <aekrosney@outlook.com>
2023-03-09 15:17:38 -0600

    further refactor EmbeddedTable.py

--- classes/EmbeddedTable.py
+++ classes/EmbeddedTable.py
@@ -40,7 +40,8 @@ class EmbeddedTable() :
         canvas_frame_x, canvas_frame_y, canvas_frame_w, canvas_frame_h = 0.00, 0.00, 0.98, 0.80
         self._canvas_frame = ttk.Frame(self._frame, style='Bordered.TFrame')
         self._canvas_frame.place(relx=canvas_frame_x, rely=canvas_frame_y, relwidth=canvas_frame_w, relheight=canvas_frame_h)
-        self._canvas_frame.bind("<MouseWheel>", self._canvas_v_scroll)
+        self._canvas_frame.bind("<MouseWheel>",
+            lambda e: "break" if self._canvas is None else self._canvas.yview_scroll(int(-e.delta/self._scroll_div), tk.UNITS))
 
         # set the font and measure for table spacing
         self._font = tk.font.Font(size=self._text_size)
@@ -50,14 +51,16 @@ class EmbeddedTable() :
         # create a vertical scrollbar
         # align to the right of the canvas with the same height
         v_scrollbar_x, v_scrollbar_y, v_scrollbar_w, v_scrollbar_h = 0.985, 0.000, 0.015, 0.800
-        self._v_scrollbar = ttk.Scrollbar(self._frame, orient=tk.VERTICAL, command=self._scrollbar_v_scroll)
+        self._v_scrollbar = ttk.Scrollbar(self._frame, orient=tk.VERTICAL, 
+                                          command=lambda _, x: "break" if self._canvas is None else self._canvas.yview_moveto(x))
         self._v_scrollbar.place(relx=v_scrollbar_x, rely=v_scrollbar_y, relwidth=v_scrollbar_w, relheight=v_scrollbar_h)
         self._v_scrollbar.bind("<ButtonPress-1>", self._scrollbar_v_click)
 
         # create a horizontal scrollbar
         # align beneath the table canvas with the same width
         h_scrollbar_x, h_scrollbar_y, h_scrollbar_w, h_scrollbar_h = 0.000, 0.805, 0.980, 0.015
-        self._h_scrollbar = ttk.Scrollbar(self._frame, orient=tk.HORIZONTAL, command=self._scrollbar_h_scroll)
+        self._h_scrollbar = ttk.Scrollbar(self._frame, orient=tk.HORIZONTAL,
+                                          command=lambda _, x: "break" if self._canvas is None else self._canvas.xview_moveto(x))
         self._h_scrollbar.place(relx=h_scrollbar_x, rely=h_scrollbar_y, relwidth=h_scrollbar_w, relheight=h_scrollbar_h)
         self._h_scrollbar.bind("<ButtonPress-1>", self._scrollbar_h_click)
 
@@ -71,7 +74,6 @@ class EmbeddedTable() :
         self._x_listbox.config(background=config.widget_bg_color, foreground=config.text_color, 
                                relief=config.relief, borderwidth=config.border_width)
         self._x_listbox.insert(0, self._listbox_default_string)
-        self._x_listbox.bind('<FocusOut>', lambda e: self.selection_clear(0, tk.END))
         
         # listbox for showing active y-data
         y_listbox_x, y_listbox_y, y_listbox_w = 0.75, 0.89, 0.30
@@ -83,10 +85,8 @@ class EmbeddedTable() :
         self._y_listbox.config(background=config.widget_bg_color, foreground=config.text_color, 
                                relief=config.relief, borderwidth=config.border_width)
         self._y_listbox.insert(0, self._listbox_default_string)
-        self._y_listbox.bind("<ButtonRelease-1>", self._set_active_y)
         self._y_listbox.bind("<Up>", self._listbox_key_up)
         self._y_listbox.bind("<Down>", self._listbox_key_down)
-        self._y_listbox.bind('<FocusOut>', lambda e: self.selection_clear(0, tk.END))
 
         # initialize set x-data button
         x_set_button_x, x_set_button_y, x_set_button_w, x_set_button_h = 0.14, 0.84, 0.10, 0.04
@@ -117,7 +117,6 @@ class EmbeddedTable() :
         self._indices = {}
         self._active_indices = []
         self._n_req_indices = 3
-        self._active_y_index = -1
 
     def open(self, filename: str) -> None:
         """Open the file at the passed file path."""
@@ -140,8 +139,10 @@ class EmbeddedTable() :
         if self._canvas is not None:
             self._canvas = self._canvas.destroy()
         self._table_columns = []
-        self._reset_x()
-        self._delete_y()
+        self._x_listbox.delete(0, tk.END)
+        self._x_listbox.insert(0, self._listbox_default_string)
+        self._y_listbox.delete(0, tk.END)
+        self._y_listbox.insert(0, self._listbox_default_string)
         self._df = None
         self._frame.update_idletasks()
 
@@ -165,22 +166,83 @@ class EmbeddedTable() :
                 col = y_index[2]
                 y_vals.append(self._df.iloc[y0:y1, col].to_numpy())
         return y_vals
+    
+    def _validate_active_data(self) -> bool:
+        """Verify the actively selected data to ensure it contains only numeric values."""
+        valid = True
+        # must have correct number of indices
+        if len(self._active_indices) != self._n_req_indices:
+            valid = False
+        # verify numeric contents
+        else:
+            row_start = self._active_indices[0]
+            row_end = self._active_indices[1]
+            col = self._active_indices[2]
+            if row_end <= row_start or self._df.iloc[row_start:row_end, col].isnull().any():
+                valid = False
+        # show warning message
+        if not valid:
+            s = "An invalid selection occurred. Please ensure the selected data contains only numeric values."
+            tk.messagebox.showwarning(title=None, message=s)
+            self._active_indices = []
+        return valid
 
-    def _scrollbar_h_scroll(self, *args) -> None:
-        """
-        Callback for horizontal scrollbar.
-        Moves the canvas with the scrollbar.
-        """
-        if self._canvas is not None:
-            self._canvas.xview_moveto(args[1])
+    def _set_x(self, event=None) -> None:
+        """Get the active table selections and assign to the active x-data."""
+        if self._validate_active_data():
+            # update the stored x-indices
+            self._indices['x'] = self._active_indices.copy()
+            x0 = self._indices['x'][0]
+            x1 = self._indices['x'][1]
+            col = self._indices['x'][2]
+            # delete existing text and overwrite
+            s = f'col:{col+1}; row:{x0+1}-{x1}'
+            self._x_listbox.delete(0)
+            self._x_listbox.insert(0, s)
 
-    def _scrollbar_v_scroll(self, *args) :
-        """
-        Callback for vertical scrollbar.
-        Moves the canvas with the scrollbar.
-        """
-        if self._canvas is not None:
-            self._canvas.yview_moveto(args[1])
+    def _add_y(self, event=None) -> None:
+        """Get the active table selections and append to the active y-data."""
+        if self._validate_active_data():
+            # update the stored y-indices
+            if 'y' in self._indices.keys():
+                self._indices['y'].append(self._active_indices.copy())
+            else:
+                self._indices['y'] = [self._active_indices.copy()]
+                self._y_listbox.delete(0) # delete default text
+            y0 = self._indices['y'][-1][0]
+            y1 = self._indices['y'][-1][1]
+            col = self._indices['y'][-1][2]
+            # delete existing text and overwrite
+            s = f'col:{col+1}; row:{y0+1}-{y1}'
+            self._y_listbox.insert(tk.END, s)
+
+    def _reset_x(self, event=None) -> None:
+        """Clear the currently selected x-data."""
+        if 'x' in self._indices.keys():
+            self._indices.pop('x')
+        self._x_listbox.delete(0, tk.END)
+        self._x_listbox.insert(0, self._listbox_default_string)
+
+    def _delete_y(self, event=None) -> None:
+        """Clear the most recent selected y-data."""
+        # clear the active/bottom entry in the internal data
+        if 'y' in self._indices.keys():
+            y_indices = list(self._y_listbox.curselection())
+
+            # clear selected data
+            # listbox only allows one selection
+            if len(y_indices) > 0:
+                self._indices['y'].pop(y_indices[0])
+                self._y_listbox.delete(y_indices[0])
+
+            # clear bottom entry
+            else:
+                self._indices['y'].pop()
+                self._y_listbox.delete(self._y_listbox.size()-1)
+
+            # add default string back into table
+            if self._y_listbox.size() == 0:
+                self._y_listbox.insert(0, self._listbox_default_string)
 
     def _scrollbar_h_click(self, event) :
         """
@@ -208,34 +270,21 @@ class EmbeddedTable() :
             self._canvas.yview_moveto(thumb_pos)
             self._canvas.update_idletasks()
 
-    def _canvas_v_scroll(self, event) -> None:
-        """Callback for scrolling on the canvas containing tabular data."""
-        if self._canvas is not None:
-            self._canvas.yview_scroll(int(-event.delta/self._scroll_div), tk.UNITS)
-
     def _listbox_key_up(self, event) -> None:
         """Callback for listbox parsing with the up arrow key."""
-        listbox_size = self._y_listbox.size()
-        if listbox_size > 0:
-            if self._active_y_index == -1:
-                self._active_y_index = 0
-            elif self._active_y_index > 0:
-                self._active_y_index -= 1
-            for i in range(listbox_size):
-                event.widget.select_clear(i)
-            event.widget.select_set(self._active_y_index)
+        if event.widget.size() > 0:
+            selected_indices = list(event.widget.curselection())
+            if len(selected_indices) > 0 and selected_indices[0] - 1 >= 0:
+                event.widget.select_clear(0, tk.END)
+                event.widget.select_set(selected_indices[0] - 1)
             
     def _listbox_key_down(self, event) -> None:
         """Callback for listbox parsing with the down arrow key."""
-        listbox_size = self._y_listbox.size()
-        if listbox_size > 0:
-            if self._active_y_index == -1:
-                self._active_y_index = 0
-            elif self._active_y_index < listbox_size - 1:
-                self._active_y_index += 1
-            for i in range(listbox_size):
-                event.widget.select_clear(i)
-            event.widget.select_set(self._active_y_index)
+        if event.widget.size() > 0:
+            selected_indices = list(event.widget.curselection())
+            if len(selected_indices) > 0 and selected_indices[0] + 1 < event.widget.size():
+                event.widget.select_clear(0, tk.END)
+                event.widget.select_set(selected_indices[0] + 1)
 
     def _populate(self, df: pd.DataFrame) -> None:
         """Populate the table with data given by the passed DataFrame"""
@@ -247,14 +296,16 @@ class EmbeddedTable() :
             # create canvas to display columns
             self._canvas = tk.Canvas(self._canvas_frame)
             self._canvas.config(background=config.widget_bg_color, highlightthickness=0, borderwidth=0)
-            self._canvas.bind("<MouseWheel>", self._canvas_v_scroll)
+            self._canvas.bind("<MouseWheel>",
+                lambda e: "break" if self._canvas is None else self._canvas.yview_scroll(int(-e.delta/self._scroll_div), tk.UNITS))
             # create all columns
             self._table_columns = []
             n_cols = len(df.columns)
             for i in range(n_cols):
                 column = TableColumn(self._canvas, i, justify=tk.LEFT, selectmode=tk.EXTENDED,
                                      height=0, width=0, activestyle=tk.NONE, font=self._font)
-                column.bind("<MouseWheel>", self._canvas_v_scroll)
+                column.bind("<MouseWheel>",
+                    lambda e: "break" if self._canvas is None else self._canvas.yview_scroll(int(-e.delta/self._scroll_div), tk.UNITS))
                 column.bind("<ButtonRelease-1>", self._get_table_selection)
                 for j in range(n_rows):
                     try:
@@ -302,84 +353,3 @@ class EmbeddedTable() :
             # multiple-selection logic
             elif len(rows) > 1:
                 self._active_indices = [rows[0], rows[-1]+1, col]
-
-    def _validate_active_data(self) -> bool:
-        """Verify the actively selected data to ensure it contains only numeric values."""
-        valid = True
-        # must have correct number of indices
-        if len(self._active_indices) != self._n_req_indices:
-            valid = False
-        # verify numeric contents
-        else:
-            row_start = self._active_indices[0]
-            row_end = self._active_indices[1]
-            col = self._active_indices[2]
-            if row_end <= row_start or self._df.iloc[row_start:row_end, col].isnull().any():
-                valid = False
-        # show warning message
-        if not valid:
-            s = "An invalid selection occurred. Please ensure the selected data contains only numeric values."
-            tk.messagebox.showwarning(title=None, message=s)
-            self._active_indices = []
-        return valid
-
-    def _set_active_y(self, event) -> None:
-        """Set the active y-index for data clearing."""
-        self._active_y_index = list(event.widget.curselection())[0]
-
-    def _set_x(self, event=None) -> None:
-        """Get the active table selections and assign to the active x-data."""
-        if self._validate_active_data():
-            # update the stored x-indices
-            self._indices['x'] = self._active_indices.copy()
-            x0 = self._indices['x'][0]
-            x1 = self._indices['x'][1]
-            col = self._indices['x'][2]
-            # delete existing text and overwrite
-            s = f'col:{col+1}; row:{x0+1}-{x1}'
-            self._x_listbox.delete(0)
-            self._x_listbox.insert(0, s)
-
-    def _add_y(self, event=None) -> None:
-        """Get the active table selections and append to the active y-data."""
-        if self._validate_active_data():
-            # update the stored y-indices
-            if 'y' in self._indices.keys():
-                self._indices['y'].append(self._active_indices.copy())
-            else:
-                self._indices['y'] = [self._active_indices.copy()]
-            y0 = self._indices['y'][-1][0]
-            y1 = self._indices['y'][-1][1]
-            col = self._indices['y'][-1][2]
-            # delete existing text and overwrite
-            s = f'col:{col+1}; row:{y0+1}-{y1}'
-            if len(self._indices['y']) == 1: # delete default text if the new index entry is the only entry
-                self._y_listbox.delete(0)
-            self._y_listbox.insert(0, s)
-
-    def _reset_x(self, event=None) -> None:
-        """Clear the currently selected x-data."""
-        if 'x' in self._indices.keys():
-            self._indices.pop('x')
-        self._x_listbox.delete(0)
-        self._x_listbox.insert(0, self._listbox_default_string)
-
-    def _delete_y(self, event=None) -> None:
-        """Clear the most recent selected y-data."""
-        # clear the active/top entry in the internal data
-        if 'y' in self._indices.keys() and len(self._indices['y']) > 0: 
-            if self._active_y_index == -1:
-                self._indices['y'].pop()
-            else:
-                y_len = len(self._indices['y'])
-                self._indices['y'].pop(y_len - self._active_y_index - 1) # listbox uses reverse order of indices than internal list
-        # clear the active/top entry in the listbox
-        if self._active_y_index == -1:
-            self._y_listbox.delete(0)
-        else:
-            self._y_listbox.delete(self._active_y_index)
-        # insert default text if listbox is empty
-        if self._y_listbox.size() == 0:
-            self._y_listbox.insert(0, self._listbox_default_string)
-        # reset active index
-        self._active_y_index = -1
